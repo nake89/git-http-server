b3d197660a6c6aba2cca480b340170c7bbde0741
Kevin Kivi <nake89@gmail.com>
2025-04-11 16:30:04 +0300

    feat: ✨ Added git push feature

--- src/main.ts
+++ src/main.ts
@@ -120,6 +120,41 @@ router.post("/push", async (ctx) => {
   ctx.response.body = { ok: 1 };
 });
 
+router.post("/pull", async (ctx) => {
+  const token = ctx.response.headers.get("X-Auth-Token");
+  if (!token) {
+    ctx.response.body = { ok: 0, error: "No token" };
+    return;
+  }
+  const username = getUsername(token);
+  const email = getEmail(token);
+  const { dirName } = await ctx.request.body.json();
+  const cwd = `${pathPrefix}/${gitDirName}/${username}/${dirName}`;
+  const repo = git({
+    cwd,
+    config: { user: { name: username, email: email } },
+  });
+  await repo.commits.pull();
+  ctx.response.body = { ok: 1 };
+})
+
+router.post("/clone", async (ctx) => {
+  const token = ctx.response.headers.get("X-Auth-Token");
+  if (!token) {
+    ctx.response.body = { ok: 0, error: "No token" };
+    return;
+  }
+  const username = getUsername(token);
+  const email = getEmail(token);
+  const { dirName } = await ctx.request.body.json();
+  const cwd = `${pathPrefix}/${gitDirName}/${username}/${dirName}`;
+  const repo = git({
+    cwd,
+    config: { user: { name: username, email: email } },
+  });
+  await repo.clone(repo.path());
+})
+
 router.post("/file", async (ctx) => {
   const formData = await ctx.request.body.formData();
   let dirName = "";
@@ -149,25 +184,26 @@ router.post("/file", async (ctx) => {
   ctx.response.body = "files uploaded";
 });
 
-router.get("/clone", async (ctx) => {
-  console.log("GET REQUEST");
+// old way
+// router.get("/clone", async (ctx) => {
+//   console.log("GET REQUEST");
 
-  // const asd = await testSsh();
-  // console.log(asd)
-  let res = "";
-  const { code, stdout, stderr } = await run("git", [
-    "clone",
-    "git@codeberg.org:nake89/testrepo.git",
-  ]);
-  console.log(code);
-  console.log(stdout);
-  console.log(stderr);
-  console.log(stdout.length);
-  console.log(stderr.length);
-  // if (stderr) res = stderr;
-  // if (stdout) res = stdout;
-  ctx.response.body = "asdsadsasd";
-});
+//   // const asd = await testSsh();
+//   // console.log(asd)
+//   let res = "";
+//   const { code, stdout, stderr } = await run("git", [
+//     "clone",
+//     "git@codeberg.org:nake89/testrepo.git",
+//   ]);
+//   console.log(code);
+//   console.log(stdout);
+//   console.log(stderr);
+//   console.log(stdout.length);
+//   console.log(stderr.length);
+//   // if (stderr) res = stderr;
+//   // if (stdout) res = stdout;
+//   ctx.response.body = "asdsadsasd";
+// });
 
 async function testSsh() {
   const { stderr, stdout } = await run("ssh", ["-T", "git@codeberg.org"]);
